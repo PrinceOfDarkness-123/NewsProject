09f88b5f4da50c6ba3de18e5c1699f31c60fabf3
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2023-09-06 16:14:53 +0500

    Removed one of a line from delete-post.php

--- include/Dashboard Panel/delete-post.php
+++ include/Dashboard Panel/delete-post.php
@@ -6,8 +6,7 @@ require("../database.php");
 $id = $_GET['id'];
 $categories_id = $_GET['category_id'];
 $sql = "Delete FROM news WHERE id= '$id';";
-$sql.= "UPDATE categories cover = cover - 1 WHERE category_id = '$categories_id'";
-if ($conn->multi_query($sql) == TRUE) {
+if ($conn->query($sql) == TRUE) {
     header("Location: $url/include/Dashboard Panel/posts.php");
     ob_end_flush();
 
